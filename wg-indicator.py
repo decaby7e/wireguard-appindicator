9905bd13c036043ef8a71a6264af1e41885b3858
decaby7e <decaby7e@ranvier.net>
2020-01-27 23:22:48 -0500

    Major changes

--- wg-indicator.py
+++ wg-indicator.py
@@ -45,8 +45,17 @@ from gi.repository import GLib
 from gi.repository import Notify as notify
 
 APPINDICATOR_ID = 'wireguard_indicator'
+BASE_DIR = os.path.expanduser('~/.config/wg-indicator')
+CONFIG = BASE_DIR + '/config.json'
+pid_file = BASE_DIR + '/wgindicator.pid'
+
+# Create the base directory if it doesn't exist
+if not os.path.exists(BASE_DIR):
+    os.mkdir(BASE_DIR)
 
 class Indicator():
+    interface = 'wg0'
+    
     def __init__(self):
         self.indicator = appindicator.Indicator.new(APPINDICATOR_ID, "network-offline", appindicator.IndicatorCategory.SYSTEM_SERVICES)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
@@ -78,14 +87,15 @@ class Indicator():
         menu.append(stop_vpn)
         menu.append(seperator)
         menu.append(self.vpn_ip)
+
         menu.show_all()
         return menu
 
     def up_vpn(self, source):
-        os.system("nohup sudo wg-quick up wg0 >/dev/null 2>&1")
+        os.system("nohup sudo wg-quick up {0} >/dev/null 2>&1".format(self.interface))
 
     def down_vpn(self, source):
-        os.system("nohup sudo wg-quick down wg0 >/dev/null 2>&1")
+        os.system("nohup sudo wg-quick down {0} >/dev/null 2>&1".format(self.interface))
 
     def copy_ip(self, source):
         clipboard = gtk.Clipboard.get(gdk.SELECTION_CLIPBOARD)
@@ -93,8 +103,8 @@ class Indicator():
 
     def get_ip(self):
         try:
-            ni.ifaddresses('wg0')
-            return ni.ifaddresses('wg0')[ni.AF_INET][0]['addr']
+            ni.ifaddresses(self.interface)
+            return ni.ifaddresses(self.interface)[ni.AF_INET][0]['addr']
         except:
             return 'Unknown'
 
@@ -102,7 +112,7 @@ class Indicator():
         while True:
             time.sleep(1)
 
-            if len(subprocess.check_output(['sudo', 'wg'])) == 0:
+            if len(subprocess.check_output(['sudo', 'wg', 'show', self.interface])) == 0:
                 GLib.idle_add(
                         self.indicator.set_icon,
                         "network-offline",
@@ -118,51 +128,55 @@ class Indicator():
                 self.vpn_ip.set_label('IP: ' + self.get_ip())
 
 # Basic service management: Only allows once instance of this to run at a time
-def service():
-	if len(sys.argv) < 2:
-		print('ERROR: Missing argument. Specify \'up\' or \'down\'')
-		sys.exit()
+def daemonize(action):
+    global pid_file
 
-	if sys.argv[1] == 'up':
-		if path.exists('app.run'):
-			print('ERROR: Service already running. (Remove app.run if this is a mistake)')
+    if action == 'up':
+        try:
+            open(pid_file, 'r')
+            print('[ Info ] Service already running. Run with `clean` if this is a mistake.')
+            hard_exit()
 
-			sys.exit()
-		else:
-			pid_file = open('app.run', 'w')
-			pid_file.write( str(os.getpid()) )
-			pid_file.close()
+        except IOError:
+            open_pid = open(pid_file, 'w')
+            open_pid.write( str(os.getpid()) )
+            open_pid.close()
 
-	elif sys.argv[1] == 'down':
-		pid_file = open('app.run', 'r')
-		pid = pid_file.readline()
-		os.system( "kill {0}".format(pid) )
-		pid_file.close()
+    elif action == 'down':
+        try:
+            open_pid = open(pid_file, 'r')
+            pid = open_pid.readline()
+            os.kill(int(pid), 15)
+            open_pid.close()
+            sys.exit()
 
-		os.system("rm app.run")
-
-		sys.exit()
-	
-	else:
-		print('ERROR: Invalid argument. Specify \'up\' or \'down\'')
-
-		sys.exit()
-
-def sigint_handler(signal_received, frame):
-	print('SIGINT detected. Exiting gracefully')
-	os.system("rm app.run")
-	sys.exit()
+        except IOError:
+            print('[ Info ] Service not running. Run with `clean` if this is a mistake.')
+            hard_exit()
+    
+    else:
+        raise ValueError
 
 def exit_handler():
-	print('Exit signal detected. Exiting gracefully')
-	os.system("rm app.run")
-	sys.exit()
+    print('[ Info ] Exit signal detected. Exiting gracefully...')
+
+    try:
+        os.remove(pid_file)
+    except IOError:
+        print('[ Error ] PID file not found. Is the indicator running?')
+
+def hard_exit():
+    atexit.unregister(exit_handler)
+    sys.exit()
 
 if __name__ == '__main__':
-	signal(SIGINT, sigint_handler)
-	atexit.register(exit_handler)
-	
-	service()
-	
-	Indicator()
-	gtk.main()
+    if len(sys.argv) < 2:
+        print('[ Error ] Missing argument. Specify \'up\' or \'down\'')
+        sys.exit()
+
+    atexit.register(exit_handler)
+
+    daemonize(sys.argv[1])
+    
+    Indicator()
+    gtk.main()
